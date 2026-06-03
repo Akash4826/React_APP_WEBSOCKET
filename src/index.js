f17f43ab5bb1e443ff5e97a384967a5996977899
Akash4826 <akashkumar482@gmail.com>
2026-06-04 00:47:47 +0530

    Added changes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import {createRoot} from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from "react-router";
+import { HashRouter } from "react-router";
 import { Provider } from 'react-redux';
 import { store } from "./store/store"
 
@@ -11,9 +11,9 @@ createRoot(document.getElementById('root')).render(
 
   <StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </StrictMode>
 
